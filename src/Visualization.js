72fef1de6e876a35f322290c465948d7aa1b1a35
Katherine Xiong <kxiong22@mit.edu>
2020-09-19 13:14:49 -0400

    styled recipe

--- src/Visualization.js
+++ src/Visualization.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import {Accordion, Card, Button} from 'react-bootstrap';
 
 export class Visualization extends React.Component {    
     render() {
         const {ingredients, instructions, title, image} = this.props.recipe;
-        const {data} = this.props;
-        console.log(ingredients);
-        console.log(instructions);
-        
+        const {nutrients} = this.props.data;
+
         var formattedIngreds = [];
         for (let i=0; i<ingredients.length; i++) {
             formattedIngreds.push(<li className="modal-words">{ingredients[i]}</li>);
@@ -17,16 +16,26 @@ export class Visualization extends React.Component {
         }
         return (
             <div>
-                <div>{data.title}</div>
-                <div>Calories: {data.nutrients.calories}</div>
-                <div className="recipe-modal-container">
-                <div className="modal-title">{title}</div>
-                <div style={{textAlign: 'center'}}><img alt="pic" src={image}/></div>
-                <div className="modal-title">Ingredients</div>
-                <div><ul>{formattedIngreds}</ul></div>
-                <div className="modal-title">Instructions</div>
-                <div><ol>{formattedInstructions}</ol></div>  
-            </div>
+                <div>{this.props.data.title}</div>
+                <Accordion>
+                    <Card>
+                        <Card.Header style={{textAlign: 'center'}}>
+                        <Accordion.Toggle as={Button} variant="info" eventKey="0">
+                           Suggested Recipe
+                        </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div className="modal-title">{title}</div>
+                                <div style={{textAlign: 'center'}}><img alt="pic" src={image}/></div>
+                                <div className="modal-title">Ingredients</div>
+                                <div><ul>{formattedIngreds}</ul></div>
+                                <div className="modal-title">Instructions</div>
+                                <div><ol>{formattedInstructions}</ol></div>  
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
             </div>
         )
     }
