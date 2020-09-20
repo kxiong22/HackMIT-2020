a144376a38c2c3ee49ea682fc90c4f227af2ecfd
Katherine Xiong <kxiong22@mit.edu>
2020-09-19 23:46:07 -0400

    added farmers markets map

--- src/Farms.js
+++ src/Farms.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Button, Modal, Col, Row, Dropdown,DropdownButton} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import './index.css';
 import axios from 'axios';
-
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
 
 
 export class Farms extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            farms: []
+            markers: [],
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        };
+    }
+
+    onMarkerClick = (props, marker, e) => {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+    }
+
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
         }
     }
 
@@ -20,9 +40,12 @@ export class Farms extends React.Component {
         try {
             const res = await axios.get(BASE_URL);
             const farms = res.data.results;
+            let marketdetails = [];
             for (let farm of farms) {
-                this.getDetailedFarmInfo(farm.id);
+                let details = await this.getDetailedFarmInfo(farm.id);
+                marketdetails.push(details);
             }
+            return marketdetails;
         } catch (e) {
             console.error(e);
         }                
@@ -33,33 +56,64 @@ export class Farms extends React.Component {
 
         try {
             const res = await axios.get(BASE_URL);
-            this.setState({farms: this.state.farms.concat(res.data.marketdetails)});
-            console.log(res.data.marketdetails);
+            return res.data.marketdetails;
+            // this.setState({farms: this.state.farms.concat(res.data.marketdetails)});
         } catch (e) {
             console.error(e);
         }
     }
 
-    render() {
-        let ans = [];
-        for (let farm of this.state.farms) {
-            ans.push(<div>
-                <div>{farm.Address}</div>
-                <div>{farm.GoogleLink}</div>
-                <div>{farm.Products}</div>
-                <div>{farm.Schedule}</div>
-            </div>)
+    async componentDidMount() {
+        let marketdetails = await this.handleFarmSearch();
+        let markers = [];
+        for (let farm of marketdetails) {
+            let indLon = farm.GoogleLink.indexOf("%2C%20");
+            let indLat = farm.GoogleLink.indexOf("?q=");
+            let lon = farm.GoogleLink.substring(indLon + 6, indLon + 13);
+            let lat = farm.GoogleLink.substring(indLat + 3, indLat + 10);
+
+            let name = <div><div>{farm.Address}</div>
+                        <div style={{fontSize: '10px', fontWeight: '300'}}>{farm.Products}</div>
+                        </div>
+
+            markers.push(<Marker name={name} 
+                                position={{lat: lat, lng: lon}} 
+                                onClick={this.onMarkerClick}>
+                <InfoWindow marker={this.state.activeMarker} 
+                            visible={this.state.showingInfoWindow}>
+                    <div><h1>{this.state.selectedPlace.name}</h1></div>
+                </InfoWindow>
+            </Marker>)
         }
 
+        this.setState({markers: markers});
+    }
+
+    render() {
         return (   
-            <div>
-                <Button onClick={() => this.handleFarmSearch()} variant="primary">Find Farms!</Button>
-                <div>{ans}</div>
+            <div style={{textAlign: 'center', marginTop: '40px'}}>
+                <Map 
+                    google={this.props.google}
+                    onClick={this.onMapClicked}
+                    style={{width: '700px', height: '500px', margin: '0 auto'}}
+                    zoom={12}
+                    initialCenter={{
+                        lat: 42.3736,
+                        lng: -71.1097
+                    }}
+                >{this.state.markers}
+                <InfoWindow marker={this.state.activeMarker} 
+                                visible={this.state.showingInfoWindow}>
+                        <div><h6>{this.state.selectedPlace.name}</h6></div>
+                </InfoWindow>
+                </Map>
             </div>             
         )
     }
 }
 
-export default Farms
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAjLkTo8Pl1lhIhIGdisIlkzqJq_HScqB4'
+})(Farms);
 
 
